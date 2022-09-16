e8af80333aadcc16d1556c8be6b090c17f4a38cf
Aishwarya Iyengar <aiyengar@thinkport.digital>
2022-09-16 11:11:28 +0200

    added function,host json files and init python file

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,16 @@
+import azure.functions as func
+import logging
 from flask import Flask
+
 app = Flask(__name__)
+
+
+def main(request: func.HttpRequest):
+    logging.info(request.method)
+
+    if request.method == "POST":
+
+        try:
+            name = request.get_json()['name']
+        except (ValueError, KeyError):
+            return func.HttpResponse("Wrong json")
